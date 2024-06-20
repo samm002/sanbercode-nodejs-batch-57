c3ae2f64c61817b0428fe2949e848d457644e868
samm002 <samuelsdjodis@gmail.com>
2024-06-20 21:58:23 +0700

    Submit Tugas Day 9

---
+++ day-9/src/routes.ts
@@ -0,0 +1,28 @@
+import express from "express";
+
+import uploadMiddleware from "./middlewares/upload.middleware";
+import uploadController from "./controllers/upload.controller";
+import productsController from "./controllers/products.controller";
+import categoriesController from "./controllers/categories.controller";
+
+const router = express.Router();
+
+// CRUD Products
+router.get("/products", productsController.findAll);
+router.post("/products", productsController.create);
+router.get("/products/:id", productsController.findOne);
+router.put("/products/:id", productsController.update);
+router.delete("/products/:id", productsController.delete);
+
+// CRUD Categories
+router.get("/categories", categoriesController.findAll);
+router.post("/categories", categoriesController.create);
+router.get("/categories/:id", categoriesController.findOne);
+router.put("/categories/:id", categoriesController.update);
+router.delete("/categories/:id", categoriesController.delete);
+
+// Route File Upload
+router.post("/upload", uploadMiddleware.single, uploadController.single);
+router.post("/uploads", uploadMiddleware.multiple, uploadController.multiple);
+
+export default router;
